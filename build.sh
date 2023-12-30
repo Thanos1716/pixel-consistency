e8ba1e851d1f27cd370d434e39ccc9cc9dcedf09
Thanos <thanos@archtop.localdomain>
2023-12-30 23:56:49 +0000

    reformatted build script again

--- build.sh
+++ build.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
-rm build/* -r
-
-mkdir build
-cp source/pack.png build
-cp source/pack.mcmeta build
-cp source/LICENSE.txt build
-cp source/assets build -r
-
-#cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# rm releases/*
-#cp pack_template.mcmeta pack.mcmeta
-#sed -i sed -i '' "s/%PACK_FORMAT%/7/g" pack.mcmeta
-
-# set -x
-
-# cd build/assets
-# pwd
-# recursively_cd
-# find build/assets -maxdepth 1 -type f | while read file; do cp -f $(readlink $file) $file; done
-
+rsync -a --progress --delete --copy-links source/pack/ build
 rsync -a --progress --delete ./build/ ../../22/pixel-consistency
-
