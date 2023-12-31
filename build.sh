a238d680c2aedd3bb0fc5355de28fe802e56390b
Thanos <thanos@archtop.localdomain>
2023-12-31 00:24:31 +0000

    remove double print of changes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rsync -a --progress --delete --copy-links source/pack/ build
-rsync -a --progress --delete ./build/ ../../22/pixel-consistency
+rsync -a --delete ./build/ ../../22/pixel-consistency
