aa923fd0a13f4d2ccbd32b3c0d7535e2e37ee73e
Thanos <thanos@archtop.localdomain>
2024-03-11 20:48:46 +1100

    release b0.4.1

--- zip_releases.sh
+++ zip_releases.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-./build.sh
+python3 build.py
 
 # cat VERSION_INFO.txt
 pack_version=$(cat VERSION_INFO.txt | awk '{print $1}')
 game_version=$(cat VERSION_INFO.txt | awk '{print $2}')
 
-cd build
+cd build/pack
 
-path="../releases/pixel-consistency-${pack_version}-${game_version}.zip"
+path="../../releases/pixel-consistency-${pack_version}-${game_version}.zip"
 
 if [ -e ${path} ]; then
     echo "File ${path} already exists"
